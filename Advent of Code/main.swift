ada66ccf5845540124a5a32c6b43fe9bd442cb5c
Dimi Chakarov <dimitar@dchakarov.com>
2022-12-07 00:43:12 +0000

    part 1

--- Advent of Code/main.swift
+++ Advent of Code/main.swift
@@ -10,30 +10,22 @@ func main() {
     let fileUrl = URL(fileURLWithPath: "./aoc-input")
     guard let inputString = try? String(contentsOf: fileUrl, encoding: .utf8) else { fatalError("Invalid input") }
     
-    let lines = inputString.components(separatedBy: "\n")
-        .filter { !$0.isEmpty }
-    
-    // Sample algorithm
-    var scoreboard = [String: Int]()
-    lines.forEach { line in
-        let (name, score) = parseLine(line)
-        scoreboard[name] = score
-    }
-    scoreboard
-        .sorted { lhs, rhs in
-            lhs.value > rhs.value
-        }
-        .forEach { name, score in
-            print("\(name) \(score) pts")
-        }
-}
+    let line = inputString.components(separatedBy: "\n")
+        .filter { !$0.isEmpty }.first!
 
-func parseLine(_ line: String) -> (name: String, score: Int) {
-    let helper = RegexHelper(pattern: #"([\-\w]*)\s(\d+)"#)
-    let result = helper.parse(line)
-    let name = result[0]
-    let score = Int(result[1])!
-    return (name: name, score: score)
+    var buffer = [Character]()
+    var currentIndex = 1
+    for char in line {
+        buffer.append(char)
+        if buffer.count > 4 {
+            buffer.removeFirst()
+            if buffer.count == Set(buffer).count {
+                print(currentIndex)
+                exit(0)
+            }
+        }
+        currentIndex += 1
+    }
 }
 
 main()
